0af8567dcdf1212fdbe94c33d251418e83bb6abd
DmytroMishenin <99320029+DmytroMishenin@users.noreply.github.com>
2022-07-16 10:36:41 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab1</title>
+    <script src="script.js"></script>
+</head>
+<body>
+<form action="" method="post" id="processor">
+    <input type="text" name="processor" placeholder="Процессор">
+    <input type="submit"><br>
+</form>
+<br>
+<form action="" method="post" id="software">
+    <select name="software">
+        <option value="Google Chrome">Google Chrome</option>
+        <option value="Opera">Opera</option>
+        <option value="FireFox">FireFox</option>
+    </select>
+    <input type="submit"><br>
+</form>
+<br>
+<form action="" method="post" id="guarantee">
+    <input hidden name="guarantee">
+    <input type="submit" value="Проверить гарантию"><br>
+</form>
+<br>
+<div id="content">
+
+</div>
+</body>
+</html>
+
